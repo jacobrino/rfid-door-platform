d4c907dee78c295865063c896ad66462996d8581
jacob_rino <jacobrino05@gmail.com>
2026-04-12 18:22:49 +0300

    Add constructor for models

---
+++ app/models/__init__.py
@@ -0,0 +1,7 @@
+from app.models.role import Role
+from app.models.staff_user import StaffUser
+from app.models.authorized_user import AuthorizedUser
+from app.models.rfid_card import RfidCard
+from app.models.rfid_assignment import RfidAssignment
+from app.models.device import Device
+from app.models.access_log import AccessLog
