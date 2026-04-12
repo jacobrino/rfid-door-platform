3b14f4eecf5b5375aa6a5d9662de0aea20525f66
jacob_rino <jacobrino05@gmail.com>
2026-04-12 18:20:44 +0300

    Add core for security

---
+++ app/core/security.py
@@ -0,0 +1,26 @@
+import secrets
+
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def generate_device_token(length: int = 32) -> str:
+    return secrets.token_urlsafe(length)
+
+
+def hash_device_token(token: str) -> str:
+    return pwd_context.hash(token)
+
+
+def verify_device_token(plain_token: str, hashed_token: str) -> bool:
+    print('plain_token & hashed_token', pwd_context.verify(plain_token, hashed_token))
+    return pwd_context.verify(plain_token, hashed_token)
