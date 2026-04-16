66f3cf4dbe92fe225daf9e3b3bc3fb8984730a26
jacob_rino <jacobrino05@gmail.com>
2026-04-16 15:49:35 +0300

    Make projet compatible to prost

--- app/core/config.py
+++ app/core/config.py
@@ -20,13 +20,20 @@ class Settings(BaseSettings):
     DIRECTORY_PATH_TEMPLATE_FILE:str
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
+    # @property
+    # def database_url(self) -> str:
+    #     return (
+    #         f"mysql+pymysql://{self.DB_USER}:{self.DB_PASSWORD}"
+    #         f"@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    #     )
+    
     @property
     def database_url(self) -> str:
         return (
-            f"mysql+pymysql://{self.DB_USER}:{self.DB_PASSWORD}"
+            f"postgresql+psycopg2://{self.DB_USER}:{self.DB_PASSWORD}"
             f"@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
         )
-    
+
     @property
     def static_path(self) -> Path:
         return Path(self.DIRECTORY_PATH_STATIC_FILE)
